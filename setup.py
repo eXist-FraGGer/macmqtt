f0a6a19c087c5e77c7c5ea48b15bb20bff66f204
Ivan Sorokin <existfragger@gmail.com>
2026-09-15 19:14:27 +0300

    bump v0.2.8

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ OPTIONS = {
         "CFBundleIdentifier": "com.macmqtt.app",
         "CFBundleName": "macmqtt",
         "CFBundleDisplayName": "macmqtt",
-        "CFBundleShortVersionString": "0.2.7",
-        "CFBundleVersion": "0.2.7",
+        "CFBundleShortVersionString": "0.2.8",
+        "CFBundleVersion": "0.2.8",
         "NSHumanReadableCopyright": "Copyright © eXist-FraGGer, 2026",
         # Menu bar only app: no Dock icon, no app switcher entry.
         "LSUIElement": True,
